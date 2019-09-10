dd1d0968a7032acf5fe2141e916008409f337acf
Mitch <mggilchrist@att.net>
2019-09-09 22:18:22 -0400

    starting code

--- main.py
+++ main.py
@@ -11,7 +11,22 @@ logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=loggin
 logger = logging.getLogger(__name__)
 
 
+def render(world,current_location):
+    '''Print out a description of the current location'''
+    room = world[current_location]
+    print(room["name"])
+    print(room["desc"])
+    return True
 
+def check_input():
+    '''Request input from the player, validate the input'''
+    user_input = input("What would you like to do?")
+    #Should I validate?
+    return user_input
+
+def update():
+    '''Check if we need to move to a new location, etc..'''
+    return True
 
 def main():
     game = {}
@@ -21,9 +36,15 @@ def main():
 
     current = 'WHOUS'
 
+    quit = False
+    while not quit:
+        #render the world
+        render(game["rooms"],current)
+        #check player input
+        user_input = check_input()
+        #update state of world
 
     return True
-
 
 
 #if we are running this from the command line, run main
